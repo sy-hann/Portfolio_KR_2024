5a05c30cbab6f01a5f78546ac0105d6a2dc7c8eb
thecoder523 <seoyon0523@gmail.com>
2021-03-17 20:25:45 +0900

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -101,3 +101,18 @@ gsap.from('.home__data', {opacity:0, duration:2, delay:.8, y:25})
 gsap.from('.home__greeting,.home__name,.home__profession,.home__button', {opacity:0, duration:2, delay:1, y:25, ease:'expo.out', stagger:.2})
 gsap.from('.nav__logo,.nav__toggle', {opacity:0, duration:2, delay:1.5, y:25, ease:'expo.out', stagger:.2})
 gsap.from('.home__social-icon', {opacity:0, duration:2, delay:2.3, y:25, ease:'expo.out', stagger:.2})
+
+// rotating cube
+var cube = document.querySelector('.cube');
+
+window.addEventListener('mousemove',function(e){
+    var x = e.clientX|0;
+    var y = e.clientY|0;
+    cube.style.transform = 'rotateX('+ -y +'deg) rotateY('+ -x +'deg)';
+})
+
+window.addEventListener('touchmove',function(e){
+    var x = e.touchers[0].clientX|0;
+    var y = e.touchers[0].clientY|0;
+    cube.style.transform = 'rotateX('+ -y +'deg) rotateY('+ -x +'deg)';
+})
